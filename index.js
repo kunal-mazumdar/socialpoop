b38d9d14ca652e87a22f8bbe7638abf8513ab2e2
KunalMZ <kunal_mazumdar@outlook.com>
2016-01-09 20:58:56 +0530

    firebase added.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var Firebase = require("firebase");
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -9,12 +10,20 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+var ref = new Firebase("https://resplendent-torch-2012.firebaseio.com/");
+
 app.get('/', function(request, response) {
   response.render('pages/index');
+});
+
+app.get('/save/:name', function(request, response) {
+  var usersRef = ref.child("users");
+    usersRef.set({
+      name: request.params.name
+    });
+    response.json({response: 'OK'});
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
