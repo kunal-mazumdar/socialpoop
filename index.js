bfbf7d300602eec9c648f75b7137ddd3670e8004
KunalMZ <kunal_mazumdar@outlook.com>
2016-02-15 23:16:19 +0530

    CORS enabled. Beta[2]

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ var ops = require('./routes/operations');
 var app = express();
 
 // Registering CORS
-app.all('/', function(req, res, next) {
+app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
